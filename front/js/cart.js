ded89ed995ff533b3b9ad4d0618ae06e92352717
Julien Desaindes <julien.desaindes@gmail.com>
2022-09-22 15:39:18 +0200

    ajout de specs

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,15 @@
 let data = JSON.parse(localStorage.data);
-console.log(data);
+console.log(data.name);
+let qty = data.qty;
+let price = data.price;
+let name = data.name;
+let color = data.color;
 
 document.querySelector(
   ".cart__item__content__description"
-).innerHTML += ` <h2>${data.name}</h2>
-<p>${data.color}</p>
-<p>${data.price} €</p>
+).innerHTML += ` <h2>${name}</h2>
+<p>${color}</p>
+<p>${price} €</p>
 `;
 
 document.querySelector(
@@ -17,12 +21,22 @@ document.querySelector(
   name="itemQuantity"
   min="1"
   max="100"
-  value= ${data.qty} 
+  value= ${qty} 
   />`;
 
-document.querySelector("#totalPrice").innerHTML += `${data.price * data.qty}`;
-
-document.querySelector("#totalQuantity").innerHTML += `${data.qty}`;
+document.querySelector("#totalQuantity").textContent = `
+  ${qty}`;
+document.querySelector("#totalPrice").textContent = `${price * qty}`;
+const selectElement = document.querySelector(
+  ".cart__item__content__settings__quantity"
+);
+selectElement.addEventListener("change", (event) => {
+  qty = event.target.value;
+  console.log(qty);
+  document.querySelector("#totalQuantity").textContent = `
+  ${qty}`;
+  document.querySelector("#totalPrice").textContent = `${price * qty}`;
+});
 
 document.querySelector(
   ".cart__item__img"
